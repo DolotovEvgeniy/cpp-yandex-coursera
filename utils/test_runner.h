98a7be7c9c89444b0e7e751b6457818cc8657e31
DolotovEvgeniy <dolotov.evgeniy@gmail.com>
2020-11-21 00:24:03 +0300

    Add task

--- utils/test_runner.h
+++ utils/test_runner.h
@@ -7,6 +7,7 @@
 #include <set>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -40,6 +41,20 @@ ostream& operator << (ostream& os, const set<T>& s) {
 
 template <class K, class V>
 ostream& operator << (ostream& os, const map<K, V>& m) {
+  os << "{";
+  bool first = true;
+  for (const auto& kv : m) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << kv.first << ": " << kv.second;
+  }
+  return os << "}";
+}
+
+template <class K, class V>
+ostream& operator << (ostream& os, const unordered_map<K, V>& m) {
   os << "{";
   bool first = true;
   for (const auto& kv : m) {
